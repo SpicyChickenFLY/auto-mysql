facfb610545c125f4292e32fa00ee6741b7844ef
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-04-25 09:34:32 +0800

    mysql pwd modify

--- utils/db/auth.go
+++ utils/db/auth.go
@@ -8,19 +8,16 @@ import (
 )
 
 const ( // MYSQL CONFIG
-
 	MySQLAllHost   = `%`
 	MySQLAllScope  = "*.*"
 	MySQLLocalHost = mysqlLocalHost
 )
 
-// ModifyMysqlPwd is a func to change passwd for user
-func ModifyMysqlPwd(db *sql.DB, passwd string) error {
+// ModifyPwdForFirstTime is a func to change passwd for user
+func ModifyPwdForFirstTime(db *sql.DB, passwd string) error {
 	// change password for user root in mysql
 	sqlStmt := fmt.Sprintf(
-		`UPDATE mysql.user SET
-		 authentication_string=password('%s')
-		 WHERE user='root';`, passwd)
+		`ALTER user user() IDENTIFIED BY '%s';`, passwd)
 	if _, err := db.Exec(sqlStmt); err != nil {
 		log.Error("execute sql successfully")
 		return err
@@ -35,8 +32,7 @@ func GrantUser(
 	user, passwd string) error {
 	// change password for user root in mysql
 	sqlStmt := fmt.Sprintf(
-		`GRANT %s ON %s
-			TO  %s IDENTIFIED BY '%s';`,
+		`GRANT %s ON %s TO  %s IDENTIFIED BY '%s';`,
 		permission, scope, user, passwd)
 	if _, err := db.Exec(sqlStmt); err != nil {
 		return err
