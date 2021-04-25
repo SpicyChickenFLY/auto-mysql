7dc882e75f7920ff1eaea1821737b1b98a9f7869
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-04-25 09:34:49 +0800

    add find process by multi args

--- utils/linux/process.go
+++ utils/linux/process.go
@@ -2,12 +2,23 @@ package linux
 
 import "fmt"
 
-// KillMysqlProcess kill all process of mysql
+// KillProcess kill all process by param
 func KillProcess(
-	s *ServerInfo, processName string) error {
+	s *ServerInfo, processParam string) error {
 	_, err := ExecuteCommand(s,
 		fmt.Sprintf(
 			"ps -ef | grep %s | grep -v grep | cut -c 9-15 | xargs kill -s 9",
-			processName))
+			processParam))
 	return err
+}
+
+// FindProcess find process by param one by one
+func FindProcess(
+	s *ServerInfo, processParams ...string) (string, error) {
+	cmdStr := "ps -ef"
+	for _, param := range processParams {
+		cmdStr += fmt.Sprintf("| grep %s | grep -v grep", param)
+	}
+	output, err := ExecuteCommand(s, cmdStr)
+	return output, err
 }
